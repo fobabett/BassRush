5aaddd556733873f7bb7dc6d8394d087a2764219
Te Vallee <astronomemusic@gmail.com>
2015-01-10 19:08:24 -1000

    enemy sprite added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,7 +7,8 @@
   var GAME_ASSET = {
     IMAGES: {
       blah: '/assets/images/mario.png',
-      bg: '/assets/images/bg32_32.png'
+      bg: '/assets/images/bg32_32.png',
+      enemy: '/assets/images/enemy.png'
     }
   };
   var game;
@@ -32,20 +33,24 @@
   function gameInit() {
     var bg = new Sprite(STAGE_WIDTH, STAGE_HEIGHT);
     player = new Sprite(400,346);
+    var enemy = new Sprite(242,242);
     player.scale(.2); //use smaller decimals for smaller sizing
     console.log(player.y);
 
 
     bg.image = game.assets[GAME_ASSET.IMAGES.bg];
     player.image = game.assets[GAME_ASSET.IMAGES.blah];
+    enemy.image = game.assets[GAME_ASSET.IMAGES.enemy];
     game.rootScene.addChild(bg);
     game.rootScene.addChild(player);
+    game.rootScene.addChild(enemy);
    
     // console.log(player,bg);
   }
   function preloadAssets() {
     game.preload(GAME_ASSET.IMAGES.bg);
     game.preload(GAME_ASSET.IMAGES.blah);
+    game.preload(GAME_ASSET.IMAGES.enemy);
   }
 
   function gameLoop(event) {
@@ -54,14 +59,16 @@
     }
     player.y = 205;
     var ground = 200;
-    player.gravity = 10;
+    player.gravity = 5;
+    // sprite jumps when up arrow is pushed
     if(game.input.up) {
       player.y = player.y - 100;
       console.log('jump test');
     }
-    // if(player.y < ground) {
-    //   player.y += player.gravity;
-    // }
+    // sprite falls to ground
+    if(player.y < ground) {
+      player.y += player.gravity;
+    }
     console.log(player.y);
   }
 
