5aca0b9ab830fe6576a1833bd55e568aaf94a8ff
Te Vallee <astronomemusic@gmail.com>
2015-01-10 18:31:01 -1000

    sprite gravity added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,7 +3,15 @@
   
   var STAGE_HEIGHT = 480;
   var STAGE_WIDTH = 640;
+  // var GROUND_Y = 450;
+  var GAME_ASSET = {
+    IMAGES: {
+      blah: '/assets/images/mario.png',
+      bg: '/assets/images/bg32_32.png'
+    }
+  };
   var game;
+  var player;
 
 
   window.onload = function(){
@@ -11,21 +19,48 @@
     enchant();
     game = new Core(STAGE_WIDTH, STAGE_HEIGHT);
     game.fps = 60;
+    game.gravity = 1;
+    // game.ground = new Sprite(STAGE_WIDTH, 200);
     preloadAssets();
     game.onload = gameInit;
+
+    game.onenterframe = gameLoop;
 
     game.start();
     
   }
   function gameInit() {
-    var player = new Sprite(290,290);
+    var bg = new Sprite(STAGE_WIDTH, STAGE_HEIGHT);
+    player = new Sprite(400,346);
+    player.scale(.2); //use smaller decimals for smaller sizing
+    console.log(player.y);
 
-    player.image = game.assets['/assets/images/blah.jpg'];
+
+    bg.image = game.assets[GAME_ASSET.IMAGES.bg];
+    player.image = game.assets[GAME_ASSET.IMAGES.blah];
+    game.rootScene.addChild(bg);
     game.rootScene.addChild(player);
-    console.log(player);
+   
+    // console.log(player,bg);
   }
   function preloadAssets() {
-    game.preload('/assets/images/blah.jpg');
+    game.preload(GAME_ASSET.IMAGES.bg);
+    game.preload(GAME_ASSET.IMAGES.blah);
   }
+
+  function gameLoop(event) {
+    if(player === undefined) {
+      return;
+    }
+    player.y = 205;
+    var ground = 200;
+    player.gravity = 10;
+
+    if(player.y < ground) {
+      player.y += player.gravity;
+    }
+    console.log(player.y);
+  }
+
 
 })();
