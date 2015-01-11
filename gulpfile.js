105ad7e25f88d98f19ffb5f107b9e7bc96187a39
Te Vallee <astronomemusic@gmail.com>
2015-01-10 16:15:12 -1000

    image added

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function () {
+  return gulp.src('./sass/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./public/styles/'));
+});
+
+gulp.task('watch_styles', function () {
+  gulp.watch('./public/styles/**/*.css', ['styles']);
+  gulp.watch('./public/scripts/**/*.js', notifyLiveReload);
+});
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+var tinylr;
+gulp.task('livereload', function() {
+  tinylr = require('tiny-lr')();
+  tinylr.listen(35729);
+});
+
+
+gulp.task('default', ['styles','watch_styles']);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,31 @@
-alert('hello world');
+ 
+(function() {
+  
+  var STAGE_HEIGHT = 480;
+  var STAGE_WIDTH = 640;
+  var game;
+
+
+  window.onload = function(){
+  
+    enchant();
+    game = new Core(STAGE_WIDTH, STAGE_HEIGHT);
+    game.fps = 60;
+    preloadAssets();
+    game.onload = gameInit;
+
+    game.start();
+    
+  }
+  function gameInit() {
+    var player = new Sprite(290,290);
+
+    player.image = game.assets['/assets/images/blah.jpg'];
+    game.rootScene.addChild(player);
+    console.log(player);
+  }
+  function preloadAssets() {
+    game.preload('/assets/images/blah.jpg');
+  }
+
+})();
